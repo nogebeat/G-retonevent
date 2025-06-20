ee66787ad6fade0d1894f55d9dde1901c7589d11
Noge <james.gbetchedji@epitech.eu>
2025-06-20 11:24:13 +0100

    remuupdate

--- frontend-web/app/crp/dashboard/page.tsx
+++ frontend-web/app/crp/dashboard/page.tsx
@@ -1,1 +1,10 @@
 'use client';
+
+export default function DashboardPage() {
+  return (
+	<div>
+	  <h1>Dashboard</h1>
+	  {/* Add your dashboard content here */}
+	</div>
+  );
+}
--- frontend-web/app/crp/notifications/page.tsx
+++ frontend-web/app/crp/notifications/page.tsx
@@ -1,1 +1,9 @@
 'use client';
+export default function DashboardPage() {
+  return (
+	<div>
+	  <h1>Dashboard</h1>
+	  {/* Add your dashboard content here */}
+	</div>
+  );
+}
